4149e0d75054a9fc501d6cc8cde24380044dc178
jony sugianto <jony.sugianto@shipper.id>
2020-12-18 14:22:52 +0700

    nambah lines

--- src/main/kotlin/fileio/writetext.kt
+++ src/main/kotlin/fileio/writetext.kt
@@ -2,9 +2,10 @@ package fileio
 
 import java.io.File
 fun main() {
-    val fileName = "/data/belajarkotlin/src/main/kotlin/fileio/abcd.txt"
+    val fileName = "/data/pandu/learnkotlin/src/main/kotlin/fileio/abcd.txt"
     var fileObject = File(fileName)
     // create a new file
     fileObject.appendText("tes tes tes tes tes tes tes datbodatbo\n")
     fileObject.appendText("tdsfsdss")
+    fileObject.appendText("fdhsufhsfhjhdstdsfsdss")
 }
